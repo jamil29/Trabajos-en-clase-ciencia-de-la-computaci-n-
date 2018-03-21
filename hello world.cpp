02b50b05011f293222401858545d2355bc423c66
jamil29 <33689725+jamil29@users.noreply.github.com>
2018-03-21 09:14:35 -0500

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -6,8 +6,13 @@ int main()
 {
     int x=5;
     int y=6;
-    int z=x+y
-    std::cout<<z<<std::endl;
-
+    int suma= x+y;
+    int multi=x*y;
+    int resta=x-y;
+    int divi=x/y;
+    
+    std::cout<<multi<<std::endl;
+    std::cout<<resta<<std::endl;
+    std::cout<<divi<<std::endl;
     return 0;
 }
