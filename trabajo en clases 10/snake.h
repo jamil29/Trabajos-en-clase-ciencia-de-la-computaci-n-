ac3a1c27ee77dc642128ef1384b02e163decda2f
jamil29 <33689725+jamil29@users.noreply.github.com>
2019-01-28 06:04:27 -0500

    Create snake.h

---
+++ trabajo en clases 10/snake.h
@@ -0,0 +1,15 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+
+
+class Snake{
+        public:
+                int x,y;
+                int dx,dy;
+                char cuerpo;
+};
+
+
+#endif // SNAKE_H
+  
